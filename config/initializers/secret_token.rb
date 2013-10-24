fd79a317bc216a289ba6b14e133bb18c738fdbfd
John Wilkinson <jwilkinson@lumoslabs.com>
2013-10-23 18:54:06 -0700

    Rails generated files (f1r$+)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SalemRecipes::Application.config.secret_token = '357a4d4be6b2e7c64891db84ccdf00668af8c805b1f189654cccd9acf9a1c534ff37d7d14d1fc1bb83f32a6379ab6fae4d8a1feb28a2f1a4c9bced5b207713d2'
